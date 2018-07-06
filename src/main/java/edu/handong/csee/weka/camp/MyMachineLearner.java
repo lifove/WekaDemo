0080522c8e8221956dcbc44ccaf890fc32ae07a3
JC <JC jc@lifove.net>
2018-07-06 14:06:05 +0900

    Update

--- src/main/java/edu/handong/csee/weka/camp/MyMachineLearner.java
+++ src/main/java/edu/handong/csee/weka/camp/MyMachineLearner.java
@@ -5,66 +5,101 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import weka.attributeSelection.BestFirst;
-import weka.attributeSelection.CfsSubsetEval;
 import weka.attributeSelection.GainRatioAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.Prediction;
+import weka.classifiers.functions.Logistic;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
-import weka.filters.unsupervised.attribute.Remove;
-import weka.filters.unsupervised.instance.RemoveRange;
 
 public class MyMachineLearner {
 
 	public static void main(String[] args) {
-		MyMachineLearner learner = new MyMachineLearner();
-		learner.run(args);
+		MyMachineLearner myTool = new MyMachineLearner();
+		
+		String[] mlAlgorithms = {"weka.classifiers.functions.Logistic",
+				"weka.classifiers.bayes.BayesNet",
+				"weka.classifiers.functions.SMO",
+				"weka.classifiers.trees.RandomForest"
+		};
+		
+		String[] dataPaths = {"Apache.arff","Safe.arff","Zxing.arff"};
+		
+		for(String mlAlg:mlAlgorithms) {
+			args[2] = mlAlg;
+			
+			args[0] = dataPaths[0];
+			args[1] = dataPaths[1];
+			myTool.run(args);
+			
+			args[0] = dataPaths[0];
+			args[1] = dataPaths[2];
+			myTool.run(args);
+			
+			args[0] = dataPaths[1];
+			args[1] = dataPaths[0];
+			myTool.run(args);
+			
+			args[0] = dataPaths[1];
+			args[1] = dataPaths[2];
+			myTool.run(args);
+			
+			args[0] = dataPaths[2];
+			args[1] = dataPaths[0];
+			myTool.run(args);
+			
+			args[0] = dataPaths[2];
+			args[1] = dataPaths[1];
+			myTool.run(args);
+		}
 	}
 
 	private void run(String[] args) {
-
-		// (1) Read Training, Test arff files
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		String arffForTraining = args[0];
-		String arffForTest = args[1];
+		
+		String trainingArffPath = args[0];
+		String testArffPath = args[1];
+		String mlAlgorithm = args[2];
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(arffForTraining));
+			// (1) Read training test files
+			BufferedReader reader = new BufferedReader(new FileReader(trainingArffPath));
 			Instances trainingData = new Instances(reader);
 			trainingData.setClassIndex(trainingData.numAttributes()-1);
 			reader.close();
-
-			reader = new BufferedReader(new FileReader(arffForTest));
+			
+			reader = new BufferedReader(new FileReader(testArffPath));
 			Instances testData = new Instances(reader);
-			testData.setClassIndex(trainingData.numAttributes()-1);
-			reader.close();
+			testData.setClassIndex(testData.numAttributes()-1);
 			
-			// (2) preprocessing
-			AttributeSelection attrSelector = getAttributesSelectionFilterByCfsSubsetEval(trainingData);
-			trainingData = selectFeaturesByAttributeSelection(attrSelector, trainingData);
-			testData = selectFeaturesByAttributeSelection(attrSelector, testData);
+			// (2) Preprocess: Feature selection
+			AttributeSelection attrSelector = getAttributesSelectionFilterByGainRatioAttributeEval(trainingData);
+			trainingData = selectFeaturesByAttributeSelection(attrSelector,trainingData);
+			testData = selectFeaturesByAttributeSelection(attrSelector,testData);
 			
-			// (3) Build a learner
-			Classifier cls = new J48();
-			cls.buildClassifier(trainingData);
-
-			// (4) Test
+			// (3) Build prediction models
+			Classifier myModel = (Classifier) weka.core.Utils.forName(Classifier.class, mlAlgorithm, null);
+			//Classifier myModel = new J48();
+			myModel.buildClassifier(trainingData);
+			
+			// (4) Apply my model on test data
 			Evaluation eval = new Evaluation(trainingData);
-			eval.evaluateModel(cls, testData);
-
+			eval.evaluateModel(myModel, testData);
+			
 			// (5) Show prediction results
 			int i=0;
-			for(Prediction prediction:eval.predictions()) {
+			/*for(Prediction prediction:eval.predictions()) {
 				String predictedValue = getClassValue(trainingData,prediction.predicted());
 				System.out.println("Instance " + (++i) + " " + predictedValue);
-			}
+			}*/
+			
+			System.out.println(trainingArffPath + "," + testArffPath + "," + mlAlgorithm+
+								"," + eval.fMeasure(0));
 
-			showSummary(eval,testData);
+			//showSummary(eval,testData);
+			
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -75,65 +110,7 @@ public class MyMachineLearner {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-	}
-
-	private void showSummary(Evaluation eval,Instances instances) {
-		for(int i=0; i<instances.classAttribute().numValues();i++) {
-			System.out.println("\n*** Summary of Class " + instances.classAttribute().value(i));
-			System.out.println("Precision " + eval.precision(i));
-			System.out.println("Recall " + eval.recall(i));
-			System.out.println("F-Measure " + eval.fMeasure(i));
 		}
-	}
-
-	String getClassValue(Instances instances, double index) {
-		return instances.attribute(instances.classIndex()).value((int) index);
-	}
-	
-	/**
-	 * Feature selection by using a specific filter (AttributeSelection)
-	 * @param filter
-	 * @param data
-	 * @return Instances instances
-	 */
-	public Instances selectFeaturesByAttributeSelection(AttributeSelection filter,Instances data) {
-		Instances newData = null;
-		
-		try {
-						// generate new data
-			newData = Filter.useFilter(data, filter);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return newData;
-	}
-	
-	/**
-	 * Feature selection filter by CfsSubsetEval
-	 * @param data
-	 * @return AttributeSelection filter
-	 */
-	public AttributeSelection getAttributesSelectionFilterByCfsSubsetEval(Instances data){
-
-		AttributeSelection filter = new AttributeSelection();  // package weka.filters.supervised.attribute!
-		CfsSubsetEval eval = new CfsSubsetEval();
-		BestFirst search = new BestFirst();
-		//search.ssetSearchBackwards(false);
-		filter.setEvaluator(eval);
-		filter.setSearch(search);
-
-		try {
-			filter.setInputFormat(data);
-
-			// generate new data
-			//newData = Filter.useFilter(data, filter);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return filter;
 	}
 	
 	/**
@@ -160,52 +137,36 @@ public class MyMachineLearner {
 	}
 	
 	/**
-	 * Get instances by removing specific attributes
-	 * @param instances
-	 * @param attributeIndices attribute indices (e.g., 1,3,4) first index is 1
-	 * @param invertSelection for invert selection, if true, select attributes with attributeIndices bug if false, remote attributes with attributeIndices
-	 * @return new instances with specific attributes
+	 * Feature selection by using a specific filter (AttributeSelection)
+	 * @param filter
+	 * @param data
+	 * @return Instances instances
 	 */
-	public Instances getInstancesByRemovingSpecificAttributes(Instances instances,String attributeIndices,boolean invertSelection){
-		Instances newInstances = new Instances(instances);
-
-		Remove remove;
-
-		remove = new Remove();
-		remove.setAttributeIndices(attributeIndices);
-		remove.setInvertSelection(invertSelection);
+	public Instances selectFeaturesByAttributeSelection(AttributeSelection filter,Instances data) {
+		Instances newData = null;
+		
 		try {
-			remove.setInputFormat(newInstances);
-			newInstances = Filter.useFilter(newInstances, remove);
+			// filter.setInputFormat(data);
+			// generate new data
+			newData = Filter.useFilter(data, filter); // package weka.filters 
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(0);
 		}
 
-		return newInstances;
+		return newData;
 	}
 
-	/**
-	 * Get instances by removing specific instances
-	 * @param instances
-	 * @param instance indices (e.g., 1,3,4) first index is 1
-	 * @param option for invert selection
-	 * @return selected instances
-	 */
-	public Instances getInstancesByRemovingSpecificInstances(Instances instances,String instanceIndices,boolean invertSelection){
-		Instances newInstances = null;
-
-		RemoveRange instFilter = new RemoveRange();
-		instFilter.setInstancesIndices(instanceIndices);
-		instFilter.setInvertSelection(invertSelection);
-
-		try {
-			instFilter.setInputFormat(instances);
-			newInstances = Filter.useFilter(instances, instFilter);
-		} catch (Exception e) {
-			e.printStackTrace();
+	String getClassValue(Instances instances, double index) {
+		//instances.attribute(11).value(0);
+		return instances.attribute(instances.classIndex()).value((int) index);
+	}
+	
+	private void showSummary(Evaluation eval,Instances instances) {
+		for(int i=0; i<instances.classAttribute().numValues();i++) {
+			System.out.println("\n*** Summary of Class " + instances.classAttribute().value(i));
+			System.out.println("Precision " + eval.precision(i));
+			System.out.println("Recall " + eval.recall(i));
+			System.out.println("F-Measure " + eval.fMeasure(i));
 		}
-
-		return newInstances;
 	}
 }
